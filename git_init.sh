095ec0520e996b4a52c7add5b0db8e986e906f61
NJUxlj <1016509070@qq.com>
2024-10-24 23:01:30 +0800

    fix git_init

--- git_init.sh
+++ git_init.sh
@@ -1,16 +1,18 @@
-eval "$(ssh-agent -s)"
-
-
-ssh-add ../.ssh/dsw-key-1
-
 
 git config --global user.email "1016509070@qq.com"
 
 
 git config --global user.name "NJUxlj"
 
+eval "$(ssh-agent -s)"
+
+
+ssh-add ../.ssh/dsw-key-1
+
 
 ssh -T git@github.com
+
+git remote set-url origin git@github.com:NJUxlj/blackprompt-bidirectional-autocot-prompt-tuning.git
 
 
 # git remote set-url origin git@github.com:NJUxlj/Travel-Agent-based-on-LLM-and-SFT.git
