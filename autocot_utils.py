ecf6128d5f1ba2bcdfee6a9436c3584df9f1a31f
NJUxlj <1016509070@qq.com>
2024-11-05 18:45:36 +0800

    update autocot

--- autocot_utils.py
+++ autocot_utils.py
@@ -6,6 +6,7 @@ Adapted from https://github.com/kojima-takeshi188/zero_shot_cot
 from statistics import mean
 from torch.utils.data import Dataset
 import openai
+from openai import OpenAI
 import os
 import multiprocessing
 import json
@@ -15,6 +16,10 @@ import re
 import random
 import time
 import datetime
+
+from typing import List, Tuple, Dict
+
+client = OpenAI()
 
 
 
@@ -36,18 +41,22 @@ def decoder_for_gpt4(args, input, max_length):
     time.sleep(args.api_time_interval)
     
     
-    if args.model == "gpt4o":
-        engine = "gpt-4-0613"
+    if args.model == "gpt-4o":
+        engine = "gpt-4o"
     
-    elif args.model == "gpt4o-mini":
-        engine = "gpt-4-32k-0613"
+    elif args.model == "gpt-4o-mini":
+        engine = "gpt-4o-mini"
     else:
-        raise ValueError("GPT model is not properly defined ... please select gpt4o or gpt4o-mini")
+        raise ValueError("GPT model is not properly defined ... please select gpt-4o or gpt-4o-mini")
     
-    if ("few_shot" in args.method or "auto" in args.method)  and engine == "code-davinci-002":
-        response = openai.Completion.create(
-          engine=engine,
-          prompt=input,
+    if ("few_shot" in args.method or "auto" in args.method): #  and engine == "code-davinci-002":
+        response = client.chat.completions.create(
+          model=engine,
+        #   prompt=input,
+          messages=[
+                        {"role": "system", "content": ""},
+                        {"role": "user", "content": input}
+                    ],
           max_tokens=max_length,
           temperature=args.temperature,
           top_p=1,
@@ -56,15 +65,153 @@ def decoder_for_gpt4(args, input, max_length):
           stop=["\n"]
         )
     else:
-        response = openai.Completion.create(
-            engine=engine,
-            prompt=input,
+        response = client.chat.completions.create( # zero-shot, zero-shot-cot
+            model=engine,
+            # prompt=input,
+            messages=[
+                        {"role": "system", "content": ""},
+                        {"role": "user", "content": input}
+                    ],
             max_tokens=max_length,
             temperature=args.temperature,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
-            stop=None
+            stop=None # 生成文本遇到该字符时停止，None则不设置停止条件 
         )
 
-    return response["choices"][0]["text"]
+    # return response["choices"][0]["text"]
+    return response.choices[0].message['content']
+
+
+
+
+class Decoder():
+    def __init__(self):
+        pass
+    
+    def decode(self, args, input, max_length):
+        response = decoder_for_gpt4(args, input, max_length)
+        return response
+    
+    
+
+
+
+def data_reader(args):
+    pass
+
+
+
+
+
+
+
+
+
+# Create dataset object before dataloader ...
+class MyDataset(Dataset):
+    pass
+
+
+
+
+
+
+def setup_data_loader(args):
+    pass
+
+
+
+
+
+
+def answer_cleansing(args, pred, must_choice=False):
+
+    print("prediction value before cleaning : " + pred)
+
+
+    if args.method in ("few_shot", "few_shot_cot", "auto_cot"):
+        # split 之前： "The answer is: 1 or The answer is: 2 or 3 or 4 or 5"
+        # preds after split = ["1 or", "2 or 3 or 4 or 5" ]
+        preds = pred.split(args.direct_answer_trigger_for_fewshot) # trigger: "The answer is:"
+        # True, 表示模型输出的结果中包含了多个候选答案
+        answer_flag = True if len(preds) > 1 else False 
+        pred = preds[-1] # 挑最后一个候选答案  "2 or 3 or 4 or 5"
+
+    
+    if args.dataset == "race":
+        pred:List = re.findall(r'A|B|C|D|E', pred) # ['2', '3', '4', '5']
+    elif args.dataset == "record":
+        pred = re.findall(r'A|B|C|D|E|F', pred)
+    elif args.dataset == "multirc":
+        pred = re.findall(r'A|B|C', pred)
+    elif args.dataset == "arc":
+        pred = re.findall(r'A|B|C', pred)
+    else:
+        raise ValueError("dataset is not properly defined ... Please select from [race, record, multirc, arc]")
+    
+    
+    # If there is no candidate in list, null is set.
+    if len(pred) == 0:
+        pred = ""
+    else:
+        if args.method in ("few_shot", "few_shot_cot", "auto_cot"):
+            if answer_flag:
+                # choose the first element in list ...
+                pred = pred[0]
+            else:
+                # choose the last element in list ...
+                pred = pred[-1]
+        elif args.method in ("zero_shot", "zero_shot_cot"):
+            # choose the first element in list ...
+            pred = pred[0]
+        else:
+            raise ValueError("method is not properly defined ... you should select from [ few_shot, few_shot_cot, zero_shot, zero_shot_cot, auto_cot ]")
+
+    
+    # for arithmetic tasks, if a word ends with period, it will be omitted ...
+    # 数学答案不能包含句号
+    if pred != "": # assume we get '78.9.'
+        if pred[-1] == ".":
+            pred = pred[:-1]
+
+    print("prediction after cleaning : " + pred)
+    
+            
+    return pred
+
+
+
+def create_demo_text(args, cot_flag)->str:
+    '''
+     将demo文件中的json对象数组拼接成一整个字符串返回
+     
+     适用于 few-shot， few-shot-cot, auto-cot
+    '''
+    x, z, y = [], [], []
+    
+    
+    with open(args.demo_path, encoding="utf-8") as f:
+        json_data = json.load(f)
+        json_data = json_data["demo"]
+        for line in json_data:
+            x.append(line["question"])
+            z.append(line["rationale"])
+            y.append(line["pred_ans"])
+    
+    index_list = list(range(len(x)))
+    
+    demo_text =""
+    
+    for i in index_list:
+        if cot_flag:
+            demo_text += x[i] + " " + z[i] + " " + \
+                         args.direct_answer_trigger_for_zeroshot_cot + " " + y[i] + ".\n\n"
+        else:
+            # args.direct_answer_trigger_for_fewshot： "The answer is:"
+            demo_text += x[i] + " " + args.direct_answer_trigger_for_fewshot + " " + y[i] + ".\n\n"
+    
+    return demo_text
+    
+    
--- run_autocot_inference.py
+++ run_autocot_inference.py
@@ -27,7 +27,7 @@ def parse_arguments():
     parser.add_argument("--max_num_worker", type=int, default=0, help="maximum number of workers for dataloader")
     
     parser.add_argument(
-        "--model", type=str, default="gpt4o", choices=["gpt4o", "gpt4o-mini", "Qwen2.5-3B"], help="model used for decoding. Select from [ gpt4o, gpt4o-mini, Qwen2.5-3B ]"
+        "--model", type=str, default="gpt4o", choices=["gpt4o", "gpt4o-mini", "Qwen2.5-3B"], help="model used for decoding. Select from [ gpt-4o, gpt-4o-mini, Qwen2.5-3B ]"
     )
     
     parser.add_argument(
@@ -39,7 +39,7 @@ def parse_arguments():
     parser.add_argument(
         "--max_length_cot", type=int, default=256, help="maximum length of output tokens by model for reasoning extraction"
     )
-    parser.add_argument(
+    parser.add_argument( # 不带推理链的答案长度 (the length after answer extraction)
         "--max_length_direct", type=int, default=32, help="maximum length of output tokens by model for answer extraction"
     )
     parser.add_argument(
@@ -58,18 +58,143 @@ def parse_arguments():
     
     args = parser.parse_args()
     
+    if args.dataset == "race":
+        args.dataset_path = "./dataset/AQuA/test.json"
+        args.direct_answer_trigger = "\nTherefore, among A through D, the answer is"
+    elif args.dataset == "multirc":
+        args.dataset_path = "./dataset/grade-school-math/test.jsonl"
+        args.direct_answer_trigger = "\nTherefore, the answer (arabic numerals) is"
+    elif args.dataset == "record":
+        args.dataset_path = "./dataset/CommonsenseQA/dev_rand_split.jsonl"
+        args.direct_answer_trigger = "\nTherefore, among A through E, the answer is"
+    elif args.dataset == "arc":
+        args.dataset_path = "./dataset/AddSub/AddSub.json"
+        args.direct_answer_trigger = "\nTherefore, the answer (arabic numerals) is"
     
+
+    # "Therefore, the answer ..." -> "The answer ..."
+    trigger = args.direct_answer_trigger.replace("\nTherefore, ", "")
+    args.direct_answer_trigger_for_zeroshot = trigger[0].upper() + trigger[1:] # "The answer is"
+    args.direct_answer_trigger_for_zeroshot_cot = args.direct_answer_trigger
+    args.direct_answer_trigger_for_fewshot = "The answer is"
+    args.cot_trigger = "Let's think step by step."
     
-
-
-
+    return args
 
 
 
 def main():
     args = parse_arguments()
+    print('*****************************')
+    print(args)
+    print('*****************************')
+    
+    fix_seed(args.random_seed)
+    print("OPENAI_API_KEY:")
+    print(os.getenv("OPENAI_API_KEY")[0:5] + '**********')
+    
+    # Initialize decoder class (load model and tokenizer) ...
+    decoder = Decoder()
+    
+    print("setup data loader ...")
+    
+    # 给huggingface数据集做适配
     
     
     
+    if args.method == "few_shot":
+        demo = create_demo_text(args, cot_flag=False)
+    elif args.method == "few_shot_cot" or args.method == "auto_cot":
+        demo = create_demo_text(args, cot_flag=True)
+    else:
+        pass
+    
+    
+    total = 0 # 记录数据集中的总共问题数量
+    correct_list = [] # 记录 1,0,1,0 ... 的列表
+    
+    with open(args.output_dir, "a") as wp:
+
+        for i, data in enumerate(dataloader):
+            if i < args.resume_id - 1: # 断点续传， 实际需要续传的断点index = resume_id -1
+            # if i < 297:
+                continue
+            
+            output_line = {} 
+            
+            print('*************************')
+            print("{}st data".format(i+1))
+            
+            
+            # Prepare question template ...
+            x, y = data
+            x = "Q: " + x[0] + "\n" + "A:"
+            y = y[0].strip()            
+            
+            output_line["question"] = x
+            output_line["gold_ans"] = y
+            
+            # adding different answer triggers
+            if args.method == "zero_shot":
+                x= x + " " + args.direct_answer_trigger_for_zeroshot
+            elif args.method == "zero_shot_cot":
+                x= x + " " + args.cot_trigger
+            elif args.method == "few_shot": # input + demonstrations + A:
+                x = demo + x
+            elif args.method == "few_shot_cot":  # input + demonstrations + A:
+                x = demo + x
+            elif args.method == "auto_cot":
+                x = demo + x + " " + args.cot_trigger
+            else:
+                raise ValueError("method is not properly defined ... please select from [ zero_shot, zero_shot_cot, auto_cot, few_shot, few_shot_cot ]")
+
+            # use LLM to answer the question ...
+            # zero-shot, few-shot 输出长度都是 1
+            max_length = args.max_length_cot if "cot" in args.method else args.max_length_direct
+            
+            z = decoder.decode(args, x, max_length)
+            
+            output_line['rationale'] = z
+            
+            # Answer extraction for zero-shot-cot ...
+            if args.method == "zero_shot_cot":
+                # 将input+rationale+answer + "Therefore the answer is:" 重新给到模型，引导模型生成精确的答案 (e.g. A,B,C,D)
+                z2 = x + z + " " + args.direct_answer_trigger_for_zeroshot_cot # i.e. "\nTherefore, among A through D, the answer is"
+                max_length = args.max_length_direct
+                pred = decoder.decode(args, z2, max_length) # output ["A", "B", "C", "D"], 
+                print("z2 + pred = ", z2 + pred)
+            else:
+                pred = z
+                print("x + pred = ", x + pred)
+            
+            # Cleansing of predicted answer ...
+            pred = answer_cleansing(args, pred)
+            
+            
+            output_line['pred_ans'] = pred
+            output_line['wrap_que'] = x
+            
+            # 把对一个样本的完整推理过程写入到输出文件中
+            output_json = json.dumps(output_line)
+            
+            wp.write(output_json + '\n')
+            
+            
+            # Choose the most frequent answer from the list ...
+            print("pred : {}".format(pred))
+            print("GT : " + y)
+            print('*************************')
+            
+            # checking answer
+            correct = (np.array([pred])==np.array([y])).sum().item()
+            correct_list.append(correct)
+            total += 1
+            
+            if (args.limit_dataset_size != 0) and ((i+1) >= args.limit_dataset_size):
+                break
+    # Calculate accuracy ...
+    accuracy = (sum(correct_list)*1.0/total) * 100
+    print("accuracy : {}%".format(accuracy))
+            
 if __name__ == "__main__":
     main()
